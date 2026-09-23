fc1c77dff77acd0ea933c054d7adbd6773d21d3f
monkeymonkey <159556711+nonamenonon@users.noreply.github.com>
2026-09-23 17:49:40 +0700

    Create your_namebook.py

---
+++ discovery_piscine/cell07/ex00/your_namebook.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+def array_of_names(persons):
+    result = []
+    for first_name in persons:
+        last_name = persons[first_name]
+        full_name = first_name.capitalize() + " " + last_name.capitalize()
+        result.append(full_name)
+    return result
+
+persons = {
+    "jean": "valjean",
+    "grace": "hopper",
+    "xavier": "niel",
+    "fifi": "brindacier"
+}
+print(array_of_names(persons))
